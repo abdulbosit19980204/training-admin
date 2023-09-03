198774557e0bbae6b2c76f8302797f5597b320d3
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-09-03 11:48:11 +0500

    loginError

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { engine, create } from "express-handlebars"
 import mongoose from "mongoose"
+import flash from "connect-flash"
 import 'dotenv/config'
 
 import AuthRouter from "./routes/auth.js"
@@ -15,6 +16,9 @@ app.set('views', './views')
 app.use(express.static("assets"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.cookieParser('keyboard cat'));
+app.use(express.session({ cookie: { maxAge: 60000 } }));
+app.use(flash())
 app.use(AuthRouter)
 app.use(AdminRouter)
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,20 +4,41 @@ import bcrypt from "bcrypt"
 
 const router = Router()
 router.get("/login", (req, res) => {
-    res.send('Login')
+    res.render('login', {
+        title: "Login",
+        token: true,
+        loginError: 'Error'
+    })
 })
 
 
 router.get('/add-user', (req, res) => {
-    res.render('addUser')
+    res.render('addUser', {
+        addUserError: "User adding error"
+    })
 })
 
 //Posts
+router.post('/login', async(req, res) => {
+    const { email, password, remember } = req.body
+    const exsistAdmin = await User.findOne({ email })
+    if (!exsistAdmin) {
 
+        req.flash("loginError", "Admin not found")
+        return
+    }
+    const isPassEqual = await bcrypt.compare(email, exsistAdmin.email)
+    if (!isPassEqual) {
+        console.log("Password is wrong");
+        return
+    }
+    console.log(exsistAdmin);
+    res.redirect('/')
+})
 router.post('/add-user', async(req, res) => {
     const { firstName, lastName, email, password, adress, city, position, zip, admin, status } = req.body
     const hashedPassword = await bcrypt.hash(password, 10)
-    console.log(hashedPassword);
+        //  console.log(hashedPassword);
     const userData = {
         firstName: firstName,
         lastName: lastName,
@@ -31,7 +52,7 @@ router.post('/add-user', async(req, res) => {
         status: status ? 'on' : 'off',
     }
     const user = await User.create(userData)
-    console.log(user);
+        // console.log(user);
     res.redirect('/users')
 })
 
